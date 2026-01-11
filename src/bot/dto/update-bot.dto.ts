2ad4e3a6f269f54ed02e790792dc1f512910aae1
Bekzod-Toxtamuratov <bekzodtoxtamuratov@gmail.com>
2026-01-11 10:09:35 +0500

    ok

--- src/bot/dto/update-bot.dto.ts
+++ src/bot/dto/update-bot.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateBotDto } from './create-bot.dto';
 
-export class UpdateBotDto extends PartialType(CreateBotDto) {}
+export class UpdateBotDto extends PartialType(CreateBotDto) {
+	
+}
